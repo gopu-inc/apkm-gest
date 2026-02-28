edb67daae1d24532d486cbf24d6cbd99a82c7078
Mauricio-100 <ceoseshell@gmail.com>
2026-02-28 14:02:00 +0100

    Update security.h

--- include/security.h
+++ include/security.h
@@ -13,15 +13,15 @@
 #define KEYRING_PATH SECURITY_PATH "/keys"
 #define SIGNATURE_PATH SECURITY_PATH "/signatures"
 #define CONFIG_FILE SECURITY_PATH "/APKM.apkm"
-#define REPO_URL "https://raw.githubusercontent.com/gopu-inc/apkm-gest/master"
+#define REPO_OWNER "gopu-inc"
+#define REPO_NAME "apkm-gest"
 #define REPO_API "https://api.github.com/repos/gopu-inc/apkm-gest"
-#define METADATA_FILE "DATA.db"
+#define REPO_RAW "https://raw.githubusercontent.com/gopu-inc/apkm-gest/main"
+#define REPO_RELEASES "https://api.github.com/repos/gopu-inc/apkm-gest/releases"
 
 typedef struct {
     char token[512];
     char sha256[128];
-    char pgp_key[4096];
-    char ssh_key[4096];
     time_t last_update;
     int validated;
 } security_token_t;
@@ -29,10 +29,12 @@ typedef struct {
 typedef struct {
     char name[256];
     char version[64];
+    char release[16];
+    char arch[32];
     char sha256[128];
-    char signature[1024];
-    char publisher[256];
+    char url[512];
     time_t timestamp;
+    char publisher[256];
 } package_metadata_t;
 
 // Prototypes
@@ -40,12 +42,6 @@ int security_init(void);
 int security_load_token(security_token_t *token);
 int security_save_token(const security_token_t *token);
 int security_download_token(void);
-int security_validate_token(const char *token);
-int security_sign_package(const char *package_path, const char *key_path, char *signature);
-int security_verify_package(const char *package_path, const char *signature);
-int security_check_duplicate(const char *package_name, const char *version);
-int security_update_metadata(const package_metadata_t *metadata);
-int security_generate_keypair(const char *name, const char *email);
 int calculate_sha256(const char *filepath, char *output);
 void btscrypt_process(char *data, int encrypt);
 
