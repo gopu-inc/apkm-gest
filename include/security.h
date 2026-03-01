4ffd51879a57489cdc267fe04c3771d679ace64d
Mauricio-100 <ceoseshell@gmail.com>
2026-03-01 14:30:23 +0100

    Update security.h

--- include/security.h
+++ include/security.h
@@ -1,51 +1,20 @@
-#ifndef APKM_SECURITY_H
-#define APKM_SECURITY_H
+#ifndef SECURITY_H
+#define SECURITY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <time.h>
+#include "apkm.h"
 
 #define SECURITY_PATH "/usr/local/share/apkm/PROTOCOLE/security"
 #define TOKEN_PATH SECURITY_PATH "/tokens/.config.cfg"
 #define KEYRING_PATH SECURITY_PATH "/keys"
 #define SIGNATURE_PATH SECURITY_PATH "/signatures"
 #define CONFIG_FILE SECURITY_PATH "/APKM.apkm"
-#define REPO_OWNER "Mauricio-100"
-#define REPO_NAME "apkm-gest"
-#define REPO_API "https://api.github.com/repos/gopu-inc/apkm-gest"
-#define REPO_RAW "https://raw.githubusercontent.com/gopu-inc/apkm-gest/main"
-#define REPO_RELEASES "https://api.github.com/repos/gopu-inc/apkm-gest/releases"
 
-typedef struct {
-    char token[512];
-    char sha256[128];
-    time_t last_update;
-    int validated;
-} security_token_t;
-
-typedef struct {
-    char name[256];
-    char version[64];
-    char release[16];
-    char arch[32];
-    char sha256[128];
-    char url[1024];
-    time_t timestamp;
-    char publisher[256];
-} package_metadata_t;
-
-// Prototypes
 int security_init(void);
-int security_load_token(security_token_t *token);  // Charge et déchiffre
-int security_get_token(char *token_buffer, size_t buffer_size);  // Nouveau: charge à chaque appel
+int security_load_token(security_token_t *token);
+int security_get_token(char *token_buffer, size_t buffer_size);
 int security_save_token(const security_token_t *token);
 int security_download_token(void);
-int security_clear_token(void);  // Nouveau: efface de la mémoire
 int calculate_sha256(const char *filepath, char *output);
 void btscrypt_process(char *data, int encrypt);
-char* escape_json(const char *str, char *output, size_t output_size);
 
 #endif
